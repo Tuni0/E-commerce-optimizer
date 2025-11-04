af7174c89e1339d9520bc643acf579f8ecd60ff9
Wiktor M <109165203+Tuni0@users.noreply.github.com>
2025-11-04 09:52:45 +0100

    up

--- frontend/shop/src/components/Products.jsx
+++ frontend/shop/src/components/Products.jsx
@@ -5,10 +5,10 @@ import axios from "axios";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { BsBasket2 } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import wirelessHeadphonesImg from "../assets/wireless-headphones.jpg";
-import wirelessMouseImg from "../assets/wireless-mouse.jpg";
-import bluetoothSpeakerImg from "../assets/bluetooth-speaker.jpg";
-import smartWatchImg from "../assets/smart-watch.jpg";
+import wirelessHeadphonesImg from "../assets/wireless-headphones.webp";
+import wirelessMouseImg from "../assets/wireless-mouse.webp";
+import bluetoothSpeakerImg from "../assets/bluetooth-speaker.webp";
+import smartWatchImg from "../assets/smart-watch.webp";
 
 const colors = [
   { key: 1, color: "White" },
